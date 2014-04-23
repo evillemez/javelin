6f30495e6ae8a4fa37d09009c47ec067ae0bdb93
Evan Villemez <evillemez@gmail.com>
2014-04-22 23:22:32 -0400

    wip packages w/ gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+    // , concat = require('gulp-concat')
+    // , lint = require('gulp-lint')
+    // , test = require('gulp-mocha')
+;
+
+gulp.task('default', function() {
+    console.log("Hello World!");
+});
