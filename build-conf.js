ae37324353bf26bc54de910f269a2b01321e5ae5
Evan Villemez <evillemez@gmail.com>
2014-04-25 02:04:53 -0400

    basic build system implemented with gulp, next up packages

---
+++ build-conf.js
@@ -0,0 +1,17 @@
+module.exports = {
+	paths: {
+		javelinCore: [
+			'util/build/javelin.prefix',
+            'src/javelin/Javelin.js',
+            'src/javelin/Registry.js',
+            'src/javelin/Engine.js',
+            'src/javelin/Plugin.js',
+            'src/javelin/AssetLoader.js',
+            'src/javelin/Dispatcher.js',
+            'src/javelin/Environment.js',
+            'src/javelin/Component.js',
+            'src/javelin/Entity.js',
+            'util/build/javelin.suffix'
+		]
+	}
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,47 @@
 var gulp = require('gulp')
-    // , concat = require('gulp-concat')
-    // , lint = require('gulp-lint')
-    // , test = require('gulp-mocha')
+    , pkg = require('./package.json')
+    , conf = require('./build-conf.js')
+    , fs = require('fs')
+    , util = require('gulp-util')
+    , concat = require('gulp-concat')
+    , jshint = require('gulp-jshint')
+    , test = require('gulp-mocha')
+    , server = require('gulp-livereload')
+    , uglify = require('gulp-uglify')
+    , debug = require('gulp-debug')
 ;
 
-gulp.task('default', function() {
-    console.log("Hello World!");
+gulp.on('err', function(e) {
+    console.log(e.err.stack);
 });
+
+gulp.task('lint', function() {
+    gulp.src(['src/**/*.js', 'demos/**/*.js', 'fixtures/**/*.js', 'tests/**/*.js'])
+        .pipe(jshint()).pipe(jshint.reporter('default')).pipe(jshint.reporter('fail'));
+});
+
+gulp.task('test', function() {
+    gulp.src('tests/**/*.js').pipe(test({reporter: 'spec'})).on('error', util.log);
+});
+
+gulp.task('build:javelin', function() {
+    gulp.src(conf.paths.javelinCore)
+        .pipe(concat('javelin-'+pkg.version+'.core.js')).pipe(gulp.dest('build/javelin/'))
+        .pipe(concat('javelin.js')).pipe(gulp.dest('build/javelin/'));
+});
+
+gulp.task('build:fixtures', function() {
+    gulp.src(['util/build/fixtures.prefix','fixtures/**/*.js','util/build/fixtures.suffix'])
+        .pipe(concat('fixtures.js')).pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:packages', function() {
+    util.log('package build not implemented...');
+});
+
+gulp.task('minify', function() {
+    gulp.src('build/javelin/*.js').pipe(uglify()).pipe(gulp.dest('build/javelin/minified/'));
+});
+
+gulp.task('build', ['build:fixtures','build:javelin','build:packages']);
+gulp.task('default', ['lint', 'build', 'test', 'minify']);
